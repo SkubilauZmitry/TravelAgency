63393ba2f8334cbf2d4bcd03c9cebe3a9781d6cc
SkubilauZmitry <skubilow@gmail.com>
2025-04-26 17:23:21 +0300

    2

---
+++ src/js/files/script.js
@@ -0,0 +1,25 @@
+// Підключення функціоналу "Чертоги Фрілансера"
+import { isMobile } from "./functions.js";
+// Підключення списку активних модулів
+import { flsModules } from "./modules.js";
+
+const headerLangBy = document.querySelector(".header__lang-by ");
+const headerLangEng = document.querySelector(".header__lang-eng ");
+const headerLanguageImage = document.querySelector(".header__language-image");
+headerLangEng.addEventListener("click", function (event) {
+    headerLangBy.classList.toggle("active");
+    headerLanguageImage.classList.toggle("active-two");
+});
+
+document.addEventListener("click", function (event) {
+    if (!event.target.closest('.header__language')) {
+        headerLangBy.classList.remove("active");
+    }
+});
+
+document.addEventListener("keyup", function (event) {
+    if (event.code === "Escape") {
+        headerLangBy.classList.remove("active");
+    };
+});
+
